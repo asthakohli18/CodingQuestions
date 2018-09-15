d69f10654828ccc095cd37ea71038e5af539eae0
asthakohli18 <43277805+asthakohli18@users.noreply.github.com>
2018-09-15 13:38:56 -0500

    Array Rotaion problem
    https://www.geeksforgeeks.org/array-rotation/

---
+++ ArrayRotation.java
@@ -0,0 +1,68 @@
+/**
+ * Created by kohas02 on 9/15/18.
+ */
+public class ArrayRotation {
+
+    /**
+     *
+     * helper method for greatest common divisor of two numbers
+     *
+     * */
+    int gcd(int a, int b){
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+    /**
+     * function that rotates arr[] of size n by d elements.
+     * reference : geeksforgeeks
+     * METHOD 3 (A Juggling Algorithm)
+     *
+     *
+     * This is an extension of method 2. Instead of moving one by one, divide the array in different sets
+     where number of sets is equal to GCD of n and d and move the elements within sets.
+     *
+     * ex arr : 1,2,3,4,5,6,7,8,9,10,11,12
+     * n = 12
+     * d = 3 (rotate by 3 elements)
+     * GCD -> 4 (basically 4 sets)
+     *
+     *
+     * */
+    private int[] leftRotateArray(int[] arr, int d, int n){
+
+        //we need 4 times to iterate d digits amoungst the sets
+
+        //actually moving blocks of 3 for 4times; which will lead to answer
+        for(int i=0;i<gcd(n,d);i++){
+            int temp = arr[i];
+            int setStart=i;
+
+            while(true) {
+                int setEnd = setStart + d;
+                if(setEnd >=n )
+                   setEnd=setEnd-n;
+
+                if(setEnd==i)
+                    break;
+
+                arr[setStart] =arr[setEnd];
+                setStart=setEnd;
+            }
+            arr[setStart]=temp;
+
+        }
+        return arr;
+
+    }
+
+    public static void main(String args[]){
+        ArrayRotation obj = new ArrayRotation();
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12};
+        int[] arr1 = obj.leftRotateArray(arr,3,12);
+        for(int i=0;i<arr1.length;i++){
+            System.out.print(" "+arr1[i]);
+        }
+    }
+}
